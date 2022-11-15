3ab613ac9672f8e5771df531e974b50dbcc67913
robs-am <roberta.amaro89@gmail.com>
2022-11-15 17:16:10 -0300

    add cep não existente

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-async function buscaEndereco() {
-  var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
-
-  console.log(consultaCEP);
-
+async function buscaEndereco(cep) {
+  try {
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("CEP não existente!");
+    }
+    console.log(consultaCEPConvertida);
+  } catch (erro) {
+    console.log(erro);
+  }
 }
-
 buscaEndereco();
